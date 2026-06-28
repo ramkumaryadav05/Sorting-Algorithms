e3b0904286cdc4b9b94ce8efe895cd8a24da1bc6
Ramkumar yadav <ramkm802112@gmail.com>
2026-06-28 17:22:07 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -5,14 +5,12 @@ int main(){
     int arr[]={5,3,6,2,9,8,1};
     int n=sizeof(arr)/sizeof(arr[0]);
 
-    for(int i=0;i<n-1;i++){
-        int index=i;        
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[index]){
-                index=j;
+    for(int i=0;i<n-1;i++){        
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
             }
         }
-        swap(arr[i],arr[index]);
     }
     cout<<"sort array"<<endl;
     for(int i=0;i<n;i++){
